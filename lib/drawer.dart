1b5c79dba3bda4ad56d79ce147f6b39aafcc7de6
= <=>
2022-07-12 18:50:07 +0530

    drawer created

---
+++ lib/drawer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatefulWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+
+      elevation: 4,
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
